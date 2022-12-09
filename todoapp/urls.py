8e1aa39a3f5d0e2c283e6e23c2e8fc6b42312dc5
Akhil K <akhilk2091@gmail.com>
2022-12-09 20:28:06 +0530

    newfiles

---
+++ todoapp/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from todoapp import views
+urlpatterns=[
+    path("signup",views.SignUpView.as_view(),name="register"),
+    path("login",views.LoginView.as_view(),name="signin")
+]
