74bb80fc2566109dc7ed1c69e4f5578fc8756268
Akhil K <akhilk2091@gmail.com>
2023-02-27 11:30:13 +0530

    initial commit

---
+++ todoapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic import View
+
+
+class SignUpView(View):
+    def get(self,request,*args,**kwargs):
+        return render(request,"registraion.html")
